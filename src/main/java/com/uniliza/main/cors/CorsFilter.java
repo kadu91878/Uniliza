114905226b0cf540b955b6330456ec2176e38cea
kadu91878 <kadu_91878@hotmail.com>
2023-11-23 22:52:19 -0300

    feat: cors filter

---
+++ src/main/java/com/uniliza/main/cors/CorsFilter.java
@@ -0,0 +1,17 @@
+package com.uniliza.main.cors;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsFilter implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173");
+    }
+
+}
